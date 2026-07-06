80e44db53581250f1c207505623104cb5536986c
yobtrd <gajy@tutamail.com>
2026-07-06 10:21:14 +0200

    feat: SQLalchemy config

--- src/epicevent/database.py
+++ src/epicevent/database.py
@@ -2,8 +2,17 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 db_url = os.getenv("DATABASE_URL")
+if db_url is None:
+    raise RuntimeError("DATABASE_URL is not definied.")
 
 engine = create_engine(db_url)
+
+Session = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
